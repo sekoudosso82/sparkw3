5fddc248d5d2750f281ed145d69df3c7cf7ebc4f
YOUR NAME <YOUR@EMAIL.com>
2021-05-03 10:58:56 -0400

    first java commit

---
+++ src/intoJavaDev/myFirstProgram.java
@@ -0,0 +1,14 @@
+package intoJavaDev;
+
+public class myFirstProgram {
+	public static void message( String myString, int myInt) {
+		System.out.println("This is my string: " + myString);
+		System.out.println("This is my integer: " + myInt);
+	}
+	
+	public static void main( String [] args) {
+		message("Hello World",  4);
+	}
+	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module intoJavaDev {
+}
