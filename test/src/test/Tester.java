b71e27c3aba5ead4ff746d8ba74d6b9b4ebdc82c
YOUR NAME <YOUR@EMAIL.com>
2021-05-10 13:02:29 -0400

    build a small app

---
+++ test/src/test/Tester.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Tester {
+
+//	public static void doThing() {}
+	public static void main(String[] args) {
+//		doThing();
+//		for (int count  = 1 ; count< 5 ; count++) {
+//			count = count + count ;
+//			System.out.println(count);
+//		}
+//		
+//		int  co = 0 ; 
+//		while(co < 20) {
+//			System.out.println("hi");
+//		}
+		 
+//		 String t = "TEST";
+//		 System.out.println(t.equals(t.toLowerCase()));
+//		 
+//		 int a = 4 ;
+//		 System.out.println(a++);
+////		 while (true) {
+//////			 System.out.println(a++);
+////			 }
+	}
+}
